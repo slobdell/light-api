b32aa29d40c97df44cac8625b37394d3ee2fe2f5
Scott Lobdell <slobdell@uber.com>
2015-07-20 15:57:18 -0700

    turn on debug

--- light_api/settings.py
+++ light_api/settings.py
@@ -29,7 +29,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 if os.environ.get("I_AM_IN_DEV_ENV"):
     DEBUG = True
